98a359f773ab2cd3af1b41c98fadb44081b45dea
Ebrahimi Sohaib <111730844+Master4SAE@users.noreply.github.com>
2023-04-02 18:14:05 +0300

    Update kohta1-6.js

--- module-1/kohta1-6.js
+++ module-1/kohta1-6.js
@@ -36,7 +36,7 @@ switch (randomValue) {
         break;
 }
 
-document.write(name + ", you are " + house + ".");
+document.write(nam + ", you are " + house + ".");
 
 //5. Write a program that asks the user to enter a year and notifies the user whether the input year is a leap year. A year is a leap year if it is divisible by four. However, years divisible by 100 are leap years only if they are also divisible by 400. Print the result on the HTML document. (3p)
 let year = prompt("Please enter a year:");
